926d59db8ec2e40692bac1f1e0ae0407e0f7503b
Ryqnwqr <Ryanprivatewqr@gmail.com>
2025-07-13 14:25:39 +1000

    leap year detection function added

--- src/LeapYearDetection.java
+++ src/LeapYearDetection.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class LeapYearDetection {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if (isLeapYear(year)) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+        sc.close();
+    }
+
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
